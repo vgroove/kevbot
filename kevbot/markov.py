13ea5a612b23214cadbb1e85e686bc77fe4ce225
Kevin Hadaway <kevinwhadaway@gmail.com>
2017-04-27 22:17:00 -0400

    separated insert_words and insert_file

--- kevbot/markov.py
+++ kevbot/markov.py
@@ -15,7 +15,7 @@ from pymongo import MongoClient
 
 class MarkovMongo(object):
     ''' Markov Chain implementation in python with storage in MongoDB.'''
-    
+
     def __init__(self, uri=None, dbname='testdb', coll='testcoll', order=2):
         connection = MongoClient(uri)
         db = connection[dbname]
@@ -25,38 +25,41 @@ class MarkovMongo(object):
         self.punctuation = ('.', '!', '?')
         self.start_word = "<<start>>"
         self.stop_word = "<<stop>>"
-    
-    
-    def insertwords(self, filename, update=True):
+
+    def insert_file(self, filename, update=True):
+        ''' Read a file and insert_words into db. '''
+        with open(filename) as f:
+            insert_words(f.read().split())
+
+    def insert_words(self, words, update=True):
         ''' Read a file, parse into tuples, and insert into db.
         Be default, updates records instead of duplicating. '''
-        with open(filename) as f:
-            chains = {}
-            for tup in self.split(f.read().split()):
-                key = tuple(tup[:-1])
-                if key in chains:
-                    chains[key].append(tup[-1])
-                else:
-                    chains[key] = [tup[-1]]
+        chains = {}
+        for tup in self.split(words):
+            key = tuple(tup[:-1])
+            if key in chains:
+                chains[key].append(tup[-1])
+            else:
+                chains[key] = [tup[-1]]
 
-            # Don't overwrite documents if we're updating            
-            if update:
-                for i, key in enumerate(chains):
-                        self.collection.update({'key': key}, {'$set': {'key': key, 'words': chains[key], 'i': i}}, upsert=True)
-            else: # Upload all new docs
-                self.collection.insert(({'i': i, 'key': key, 'words': chains[key]} for i, key in enumerate(chains)))
-            self.size = self.collection.count()
-            
-    
-    def getwords(self, key):
+        # Don't overwrite documents if we're updating
+        if update:
+            for i, key in enumerate(chains):
+                    self.collection.update({'key': key}, {'$set': {'key': key, 'words': chains[key], 'i': i}}, upsert=True)
+        else:
+            self.collection.insert(({'i': i, 'key': key, 'words': chains[key]} for i, key in enumerate(chains)))
+        self.size = self.collection.count()
+
+
+    def get_words(self, key):
         ''' Get the list of words from the database corresponding to
         the supplied key.  Key should be a tuple containing two strings. '''
         result = self.collection.find_one({'key': key})
         while result == None:
             result = self.collection.find_one({'i': random.randint(0, self.size)})
         return result['words']
-    
-    
+
+
     def split(self, words):
         ''' Parse the supplied string into n-word chunks.  For example,
     parsing 'One small step for man' into order-3 chunks would yield:
@@ -65,7 +68,7 @@ class MarkovMongo(object):
     ('step', 'for', 'man')'''
         if len(words) < self.order + 1:
             return
-            
+
         for i in range(len(words) - self.order):
             yield words[i : i + self.order + 1]
 
@@ -92,7 +95,7 @@ class MarkovMongo(object):
 
         # Generate words and append to string until message length or stop word
         for i in range(length - self.order):
-            newword = random.choice(self.getwords(key))
+            newword = random.choice(self.get_words(key))
             if newword == self.stop_word:
                 break
             words.append(newword)
