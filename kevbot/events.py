253d34c34950d45aac7a3a708fbd2a2fd1dfcfef
Kevin Hadaway <kevinwhadaway@gmail.com>
2017-04-30 21:52:47 -0400

    moved database gens and updates to different processes

--- kevbot/events.py
+++ kevbot/events.py
@@ -1,4 +1,5 @@
 import asyncio
+from concurrent.futures import ProcessPoolExecutor
 import discord
 import logging
 from markov import MarkovMongo
@@ -6,12 +7,19 @@ import random
 import re
 
 client = discord.Client()
-mm = MarkovMongo()
+#chat_engine = MarkovMongo()
 is_url = re.compile("^(https?|ftp)://[^\s/$.?#].[^\s]*$@iS")
+executor = ProcessPoolExecutor(3)
 
-async def generate(seed=None):
-    """Async task for generating a response"""
-    return mm.generate(seed)
+def generate(seed=None):
+    """Generate a response given a seed"""
+    return MarkovMongo().generate(seed)
+
+def update_db(text):
+    """Log words into chat engine data structure"""
+    logging.info("Updating database with \"{0}\"...".format(text))
+    MarkovMongo().insert_words(text)
+    logging.info("Finished updating database with \"{0}\"...".format(text))
 
 @client.event
 async def on_read():
@@ -20,6 +28,7 @@ async def on_read():
 @client.event
 async def on_message(message):
     """Called when message is received on any channel on server"""
+
     if message.server.name == "Kevbot Test Server":
         words = message.content.split(' ')
 
@@ -28,16 +37,18 @@ async def on_message(message):
             logging.info("Responding to message: \"{0}\"".format(message.content))
             if len(words) > 6:
                 index = random.randint(0, len(words)-2)
-                response = await generate(seed=(words[index], words[index + 1]))
+                response = await client.loop.run_in_executor(executor, 
+                        generate, (words[index], words[index + 1]))
                 logging.info("Generated \"{0}\" from seed \"({1}, {2})\"".format(
                     response, words[index], words[index + 1]))
             elif len(words) > 2:
                 index = random.randint(0, len(words)-1)
-                response = await generate(seed=words[index])
+                response = await client.loop.run_in_executor(executor,
+                        generate, words[index])
                 logging.info("Generated \"{0}\" from seed \"{1}\"".format(
                     response, words[index]))
             else:
-                response = await generate()
+                response = await client.loop.run_in_executor(executor, generate)
                 logging.info("Generated \"{0}\" with no seed".format(response))
             if not is_url.match(response):
                 logging.info("Sending response...")
@@ -47,8 +58,7 @@ async def on_message(message):
 
     # Record conversation anytime someone else speaks
     if message.author.name != "Kevbot":
-        logging.info("Updating database...")
-        mm.insert_words(message.content)
+        await client.loop.run_in_executor(executor, update_db, message.content)
 
 @client.event
 async def on_reaction_add(reaction, user):
